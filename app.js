3d3d42346c82860f629e539bb30b41bc0253bdc3
Even Stensberg <evenstensberg@gmail.com>
2022-10-22 18:27:25 +0200

    chore: wip

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
+"use strict";
 
+const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8080;
+// Use the built-in express middleware for serving static files from './public'
+app.use("/", express.static("dist/steakhouse"));
 
-app.use(express.static(__dirname + "/dist/steakhouse"));
-
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
-
-const server = http.createServer(app);
-
-server.listen(port, () =>
-  console.log(`App running on: http://localhost:${port}`)
-);
+app.get("*", (req, res, next) => {
+  const indexFile = path.resolve(__dirname + "/dist/steakhouse/index.html");
+  res.sendFile(indexFile);
+});
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
