5ef14d1c58080f79ab21ef68b156021f2e44be6a
Even Stensberg <evenstensberg@gmail.com>
2024-10-11 21:50:11 +0200

    chore: add rete limiting

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 "use strict";
 
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const app = express();
 const _app_folder = "dist/steakhouse";
+
+
+const limiter = rateLimit({
+	windowMs: 20 * 60 * 1000, // 15 minutes
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  handler: function (req, res, next) {
+    res.send("For mange forsøk på kort tid. Vennligst vent en liten stund før du prøver igjen.")
+  },
+})
+
+app.use(limiter);
 
 app.use("/", express.static(_app_folder, { maxAge: 60000 }));
 
